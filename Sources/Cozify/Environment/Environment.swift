a77ce9ef8a0097b06f64b6272a832140c4b3610f
Rinat Abidullin <rinatabidullin@gmail.com>
2022-01-09 22:58:46 +0500

    Environment + Configuration

--- Sources/Cozify/Environment/Environment.swift
+++ Sources/Cozify/Environment/Environment.swift
@@ -8,14 +8,49 @@
 import Foundation
 
 public struct Environment {
-    public static var isProduction: Bool {
-        #if DEBUG
-        return false
-        #else
-        guard let path = Bundle.main.appStoreReceiptURL?.path else {
-            return true
-        }
-        return !(path.contains("CoreSimulator") || path.contains("sandboxReceipt"))
-        #endif
+    public enum Configuration {
+        case debug
+        case simulator
+        case testflight
+        case production
     }
+    
+    public static let isDebug: Bool = {
+        #if DEBUG
+        return true
+        #else
+        return false
+        #endif
+    }()
+    
+    public static let isSimulator: Bool = {
+        #if targetEnvironment(simulator)
+        return true
+        #else
+        return false
+        #endif
+    }()
+    
+    public static var isTestFlight: Bool = {
+        guard !isDebug, !isSimulator else { return false }
+        guard let path = Bundle.main.appStoreReceiptURL?.path else { return false }
+        return path.contains("sandboxReceipt")
+    }()
+    
+    public static var isProduction: Bool = {
+        guard !isTestFlight else { return false }
+        return true
+    }()
+    
+    public static let configuration: Configuration = {
+        if isDebug {
+            return .debug
+        } else if isSimulator {
+            return .simulator
+        } else if isTestFlight {
+            return .testflight
+        } else {
+            return .production
+        }
+    }()
 }
